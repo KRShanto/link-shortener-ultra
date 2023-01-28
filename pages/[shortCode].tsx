9e85387350391f87336c8de3b827d1fdccfd5c9b
KR Shanto <84588948+KRShanto@users.noreply.github.com>
2023-01-28 13:07:37 +0600

    fix bug: extra }

--- pages/[shortCode].tsx
+++ pages/[shortCode].tsx
@@ -8,7 +8,7 @@ export default function RedirectLandingPage({ host, youtubeToken }) {
   const { shortCode } = router.query;
 
   // const link = `https://www.youtube.com/redirect?event=comments&redir_token=${youtubeToken}&q=${host}/red/${router.query.shortCode}&html_redirect=1`;
-  const link = `${host}/red/${router.query.shortCode}}`;
+  const link = `${host}/red/${router.query.shortCode}`;
 
   return (
     <>
