4d559bc7cbb61dc95ed5c55506efb85e417f7f46
KR Shanto <84588948+KRShanto@users.noreply.github.com>
2023-01-31 22:56:43 +0600

    change

--- pages/[shortCode].tsx
+++ pages/[shortCode].tsx
@@ -8,8 +8,8 @@ export default function RedirectLandingPage({ host, youtubeToken }) {
   const router = useRouter();
   const { shortCode } = router.query;
 
-  const link = `https://www.youtube.com/redirect?event=comments&redir_token=${youtubeToken}&q=${host}/red/${router.query.shortCode}&html_redirect=1`;
-  // const link = `https://${host}/red/${router.query.shortCode}`;
+  // const link = `https://www.youtube.com/redirect?event=comments&redir_token=${youtubeToken}&q=${host}/red/${router.query.shortCode}&html_redirect=1`;
+  const link = `https://${host}/red/${router.query.shortCode}`;
 
   // useEffect(() => {
   //   function killPopup() {
@@ -39,9 +39,9 @@ export default function RedirectLandingPage({ host, youtubeToken }) {
             // redirect
             router.push(link);
 
-            setTimeout(() => {
-              router.push(link);
-            }, 2000);
+            // setTimeout(() => {
+            //   router.push(link);
+            // }, 2000);
           }}
         >
           Join Free
